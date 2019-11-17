a8ac4b2a10c3fab29e50154a261174b9ea87970a
amr-amr <andrei.romascanu@mail.mcgill.ca>
2019-11-16 23:06:38 -0500

    datadir for days

--- arxiv_net/__init__.py
+++ arxiv_net/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 HOME_DIR = Path.home()
 ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = f'{ROOT_DIR.parent}/data'
+DATA_DIR = ROOT_DIR.parent / "data"
