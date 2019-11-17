9a8150b5338bb08337121a231f1df928d6105d43
NicholasConnors <nicholasconnors@hotmail.ca>
2019-11-17 12:10:44 -0500

    CSS & gifs

--- arxiv_net/dashboard/pages/arxiv_dash.py
+++ arxiv_net/dashboard/pages/arxiv_dash.py
@@ -302,7 +302,7 @@ explore_feed_layout = html.Div(
         html.Div(
             id='focus-feed',
             children=[
-                html.Button('show search feed', id='hide-button'),
+                html.Button('show_search_feed', id='hide-button'),
                 html.Div('Related papers: '),
                 dcc.RadioItems(
                     id='radio',
